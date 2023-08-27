f6d82136aaa72c1b0ab7bbb82522e3cba21961a8
João Antônio <joao.antonio2452@outlook.com>
2023-08-27 17:05:51 -0300

    Novo exercicio

---
+++ curso-js/exercícios/exercicio 07/arquivo.js
@@ -0,0 +1,8 @@
+function adicionar() {
+    let num = document.getElementById('numero')
+    if (num > 100 || num < 1){
+        window.alert('Número invalido, digite um numero de 0 a 100')
+    } else {
+        
+    }
+}
