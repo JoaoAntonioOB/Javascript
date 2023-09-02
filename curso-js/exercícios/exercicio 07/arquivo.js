eccb6b9a1f3dac5545aee97147a0670a86ebeaf8
João Antônio <joao.antonio2452@outlook.com>
2023-09-02 13:24:40 -0300

    Exercícios de lógica

--- curso-js/exercícios/exercicio 07/arquivo.js
+++ curso-js/exercícios/exercicio 07/arquivo.js
@@ -1,8 +1,32 @@
-function adicionar() {
-    let num = document.getElementById('numero')
-    if (num > 100 || num < 1){
-        window.alert('Número invalido, digite um numero de 0 a 100')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = ducument.getElementById('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
     } else {
-        
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        /*valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)*/
+        window.alert('tudo ok')
+        } else {
+        window.alert('Número invalido, ou já encontrado na lista.')
     }
 }
